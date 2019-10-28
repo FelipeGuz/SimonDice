68a538cb80173e5d3e6e12047da11a64cf07e6d6
Prestamo Estudiantes <prestamour@urosario.edu.co>
2019-10-28 15:51:10 -0500

    Mejoras funcion que genera y evalua los turnos

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,23 @@
 
+
 var patron = [0];
 var intento = 0;
+var turno = 0;
 
 while(true){
     var n = prompt();
     if(partida(patron,n,intento)==true){
-        console.log("Correcto: "+patron)
+        if(intento==turno){
+            var n = Math.floor((Math.random()*4)+1);
+            patron.push(n);
+            
+            console.log("Correcto: "+n)
+            intento = 0;
+            turno++;   
+        }else{
+            console.log("Numero Correcto")
+            intento++;
+        }
     }else{
         console.log("Incorrecto");
         break;
@@ -15,9 +27,9 @@ while(true){
 
 // Confirmar que el boton oprimido es el correcto de la sucesion
 function partida(patron,boton,intento){
-    if(patron[intento]===boton){
-        var n = Math.floor((Math.random()*4)+1);
-        patron.push(n);
+    console.log("Valor esperado: "+patron[intento]);
+    console.log("Boton: "+parseInt(boton))
+    if(patron[intento]===parseInt(boton)){
         return true;
     }else{
         return false;
