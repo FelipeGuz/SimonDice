a1cf74780ddd88bbed97f3e608d755a554a18635
Biblioteca UR <bibliotecaur@urosario.edu.co>
2019-10-30 16:33:43 -0500

    creacion del enlace html con javascipt

--- Script/script.js
+++ Script/script.js
@@ -1,15 +1,25 @@
 
+var patron = [Math.floor((Math.random()*4)+1)];
 
-var patron = [0];
+var temp = document.getElementById("boton"+patron[0]).style.boxShadow;
+console.log(temp)
+// document.getElementById("boton"+patron[0]).style.boxShadow = "none";
+document.getElementById("boton"+patron[0]).style.boxShadow = temp;
+
 var intento = 0;
 var turno = 0;
 
-while(true){
-    var n = prompt();
-    if(partida(patron,n,intento)==true){
+function juego(boton){
+    if(partida(patron,boton,intento)==true){
         if(intento==turno){
             var n = Math.floor((Math.random()*4)+1);
             patron.push(n);
+
+            for(var i=0; i<patron.length; i++){
+                var temp = document.getElementById("boton"+patron[i]);
+                document.getElementById("boton"+patron[i]).style.boxShadow = "none";
+                document.getElementById("boton"+patron[i]).style.boxShadow = temp;
+            }
             
             console.log("Correcto: "+n)
             intento = 0;
@@ -23,6 +33,26 @@ while(true){
         break;
     }
 }
+
+// while(true){
+//     var n = prompt();
+//     if(partida(patron,n,intento)==true){
+//         if(intento==turno){
+//             var n = Math.floor((Math.random()*4)+1);
+//             patron.push(n);
+            
+//             console.log("Correcto: "+n)
+//             intento = 0;
+//             turno++;   
+//         }else{
+//             console.log("Numero Correcto")
+//             intento++;
+//         }
+//     }else{
+//         console.log("Incorrecto");
+//         break;
+//     }
+// }
 
 
 // Confirmar que el boton oprimido es el correcto de la sucesion
